b9b60100ad14dcf62149c8ebad9d8204174ffd3f
David <vengo53@gmail.com>
2024-01-19 19:38:10 +0100

    Interfaz basica y merge con marc

--- app/src/main/java/com/example/urfungi/MainActivity.kt
+++ app/src/main/java/com/example/urfungi/MainActivity.kt
@@ -3,44 +3,148 @@ package com.example.urfungi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.urfungi.ui.theme.UrFungiTheme
+import androidx.compose.ui.res.stringResource
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.urfungi.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContent {
-            UrFungiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            AppTheme {
+                val navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            val navBackStackEntry by navController.currentBackStackEntryAsState()
+                            val destinoActual = navBackStackEntry?.destination
+
+                            Destino.entries.forEach { destino ->
+                                val destinoSeleccionado =
+                                    destinoActual?.hierarchy?.any { it.route == destino.ruta } == true
+
+                                NavigationBarItem(
+                                    selected = destinoSeleccionado,
+                                    onClick = {
+                                        if (!destinoSeleccionado) {
+                                            navController.navigate(destino.ruta) {
+                                                popUpTo(navController.graph.findStartDestination().id)
+                                                launchSingleTop = true
+                                            }
+                                        }
+                                    },
+                                    icon = {
+                                        Icon(
+                                            imageVector = if (destinoSeleccionado) destino.iconoSeleccionado else destino.icono,
+                                            contentDescription = stringResource(id = destino.nombre)
+                                        )
+                                    },
+                                    label = {
+                                        Text(text = stringResource(id = destino.nombre))
+                                    }
+                                )
+                            }
+                        }
+                    }
+                ) { paddingValues ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = Destino.entries.first().ruta
+                        ) {
+                            composable(
+                                route = Destino.Destino1.ruta,
+                                enterTransition = {
+                                    slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+                                },
+                                exitTransition = {
+                                    slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+                                }
+                            ) {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(text = stringResource(id = Destino.Destino1.nombre))
+                                }
+                            }
+
+                            composable(
+                                route = Destino.Destino2.ruta,
+                                enterTransition = {
+                                    when (initialState.destination.route) {
+                                        Destino.Destino1.ruta -> slideIntoContainer(
+                                            AnimatedContentTransitionScope.SlideDirection.Left
+                                        )
+
+                                        else -> slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+                                    }
+                                },
+                                exitTransition = {
+                                    when (targetState.destination.route) {
+                                        Destino.Destino1.ruta -> slideOutOfContainer(
+                                            AnimatedContentTransitionScope.SlideDirection.Right
+                                        )
+
+                                        else -> slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+                                    }
+
+                                }
+                            ) {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(text = stringResource(id = Destino.Destino2.nombre))
+                                }
+                            }
+
+                            composable(
+                                route = Destino.Destino3.ruta,
+                                enterTransition = {
+                                    slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+                                },
+                                exitTransition = {
+                                    slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+                                }
+                            ) {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(text = stringResource(id = Destino.Destino3.nombre))
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UrFungiTheme {
-        Greeting("Android")
     }
 }
