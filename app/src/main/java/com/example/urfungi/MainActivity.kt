706fe4a672567096c20d342f469a4ae08f74ef66
Marc Mengual Gestí <mengual.gesti.marc@alumnat.copernic.cat>
2024-02-08 20:12:33 +0100

    Amigos, LogOut

--- app/src/main/java/com/example/urfungi/MainActivity.kt
+++ app/src/main/java/com/example/urfungi/MainActivity.kt
@@ -4,26 +4,39 @@ import MapScreen
 import android.Manifest
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -43,11 +56,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -56,6 +71,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.urfungi.Curiosidades.SetasListScreen
 import com.example.urfungi.QuizJuego.QuizScreenFromFirebase
 import com.example.urfungi.Recetas.RecetasSetasListScreen
@@ -70,6 +87,8 @@ import org.osmdroid.library.BuildConfig
 import java.io.IOException
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 
 //import androidx.compose.material.rememberCoilPainter
 
@@ -417,6 +436,8 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MensajesScreen() {
         var usuario by remember { mutableStateOf<usuarios?>(null) }
+        var amigos by remember { mutableStateOf<List<usuarios>>(emptyList()) }
+
 
         LaunchedEffect(Unit) {
             val currentUser = FirebaseAuth.getInstance().currentUser
@@ -429,6 +450,22 @@ class MainActivity : ComponentActivity() {
                     if (documentSnapshot.exists()) {
                         // El documento existe, puedes obtener el objeto usuarios
                         usuario = documentSnapshot.toObject(usuarios::class.java)
+
+                        // Obtener la lista de amigos
+                        val listaAmigos = usuario?.amigos ?: emptyList()
+
+                        // Obtener información de cada amigo y almacenarla en la lista 'amigos'
+                        listaAmigos.forEach { amigoId ->
+                            val amigoReference = db.collection("usuarios").document(amigoId)
+                            amigoReference.get().addOnSuccessListener { amigoSnapshot ->
+                                if (amigoSnapshot.exists()) {
+                                    val amigo = amigoSnapshot.toObject(usuarios::class.java)
+                                    if (amigo != null) {
+                                        amigos = amigos + amigo
+                                    }
+                                }
+                            }
+                        }
                     } else {
                         // El documento no existe
                         Log.d(
@@ -447,66 +484,258 @@ class MainActivity : ComponentActivity() {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.TopStart
         ) {
-            if (usuario != null) {
+            // Texto "Tu Perfil"
+            Text(
+                text = "Tu Perfil:",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 12.dp)
+            )
 
-                MensajesCard(usuario)
+            // Tarjeta de información del usuario
+            if (usuario != null) {
+                MensajesCard(
+                    usuario = usuario,
+                    onLogoutClick = {
+                        FirebaseAuth.getInstance().signOut()
+                        navigateToLoginAppActivity()
+                    },
+                    onEditClick = {}
+                )
             } else {
                 Text("El usuario es nulo")
+            }
+
+            // Texto "Amigos"
+            Text(
+                text = "Amigos:",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.TopStart)
+                    .offset(y = 200.dp) // Ajustar según sea necesario para evitar superposición
+            )
+
+            // Botón "Añadir Amigos"
+            Button(
+                onClick = {
+                    // Implementa la lógica para añadir amigos
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.TopStart)
+                    .offset(y = 240.dp) // Ajustar según sea necesario para evitar superposición
+            ) {
+                Text("Añadir Amigos")
+            }
+
+            // Lista de tarjetas de amigos
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 310.dp) // Ajustar según sea necesario para evitar superposición
+            ) {
+                items(amigos) { amigo ->
+                    AmigoCard(amigo = amigo)
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun AmigoCard(amigo: usuarios) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .padding(10.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Imagen de perfil del amigo a la izquierda
+                AndroidView(factory = { context ->
+                    ImageView(context).apply {
+                        val amigoFoto = amigo.foto
+                        val imageUrl = "$amigoFoto"
+                        Glide.with(context)
+                            .load(imageUrl)
+                            .fitCenter()
+                            .transform(CircleCrop())
+                            .into(this)
+                    }
+                }, modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Green, CircleShape)
+                )
+
+                // Espaciador horizontal
+                Spacer(modifier = Modifier.width(12.dp))
+
+                // Columna para el Usuario y Nombre del amigo
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .align(Alignment.CenterVertically),
+                    verticalArrangement = Arrangement.spacedBy(2.dp)
+                ) {
+                    // Nombre de usuario más pequeño del amigo
+                    Text(
+                        text = amigo.username ?: "Username",
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 8.dp) // Padding superior al username
+                    )
+
+                    // Nombre completo del amigo
+                    Text(
+                        text = amigo.nombre ?: "Nombre del Amigo",
+                        fontSize = 14.sp,
+                        color = Color.White
+                    )
+                }
             }
         }
     }
 
     @Composable
-    fun MensajesCard(usuario: usuarios?) {
+    fun MensajesCard(usuario: usuarios?, onLogoutClick: () -> Unit, onEditClick: () -> Unit) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.TopStart
         ) {
-            if (usuario != null) {
-                // Rectángulo con bordes redondeados
+            usuario?.let {
                 Card(
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(150.dp)
-                        .padding(25.dp),
+                        .padding(top = 40.dp, start = 16.dp, end = 16.dp),
+
                     shape = RoundedCornerShape(16.dp),
                 ) {
                     Row(
                         modifier = Modifier
                             .fillMaxSize()
-                            .padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
+                            .padding(6.dp),
+
+                    verticalAlignment = Alignment.CenterVertically
                     ) {
-
-                        val userFoto = usuario.foto
-                        val imageUrl = "gs://urfungui.appspot.com/usuarios/$userFoto"
-                     /*   val paint = rememberCoilPainter(imageUrl)
-
-                        Image(
-                            modifier = Modifier
-                                .size(80.dp)
-                                .clip(CircleShape)
-                                .background(MaterialTheme.colorScheme.primary),
-                            painter = paint,
-                            contentDescription = null,
-                        )*/
-
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        // Nombre del usuario
-                        Text(
-                            text = usuario.nombre ?: "Nombre del Usuario",
-                            fontSize = 22.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.White
+                        // Imagen de perfil del usuario a la izquierda
+                        AndroidView(factory = { context ->
+                            ImageView(context).apply {
+                                val userFoto = usuario.foto
+                                val imageUrl = "$userFoto"
+                                Glide.with(context)
+                                    .load(imageUrl)
+                                    .fitCenter()
+                                    .transform(CircleCrop())
+                                    .into(this)
+                            }
+                        }, modifier = Modifier
+                            .size(80.dp)
+                            .clip(CircleShape)
+                            .border(2.dp, Color.Green, CircleShape)
                         )
 
-                        Spacer(modifier = Modifier.width(8.dp))
+                        // Espaciador horizontal
+                        Spacer(modifier = Modifier.width(12.dp))
+
+                        // Columna para el Usuario y Nombre
+                        Column(
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight()
+                                .align(Alignment.CenterVertically),
+                            verticalArrangement = Arrangement.spacedBy(2.dp)
+                        ) {
+                            // Nombre de usuario más pequeño
+                            Text(
+                                text = usuario.username ?: "Username",
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White,
+                                modifier = Modifier.padding(top = 8.dp) // Padding superior al username
+                            )
+
+                            // Nombre completo del usuario
+                            Text(
+                                text = usuario.nombre ?: "Nombre del Usuario",
+                                fontSize = 14.sp,
+                                color = Color.White
+                            )
+                        }
+
+                        // Espaciador horizontal
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        // Botones a la derecha del todo
+                        Column(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                        ) {
+                            // Botón de Configuración
+                            IconButton(
+                                onClick = { onEditClick() },
+                                modifier = Modifier
+                                    .size(40.dp) // Aumentado el tamaño del botón
+                                    .clip(CircleShape)
+                                    .background(Color.Gray)
+                                    .padding(top = 8.dp, end = 8.dp)
+                            ) {
+                                Icon(imageVector = Icons.Default.Settings,
+                                    contentDescription = "Configuración",
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(4.dp)
+
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            // Botón de Logout
+                            IconButton(
+                                onClick = { onLogoutClick() },
+                                modifier = Modifier
+                                    .size(40.dp) // Aumentado el tamaño del botón
+                                    .clip(CircleShape)
+                                    .background(Color.Red)
+                                    .padding(top = 8.dp, end = 8.dp)
+
+                            ) {
+                                Icon(imageVector = Icons.Default.ExitToApp,
+                                    contentDescription = "Logout",
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(4.dp)
+
+                                )
+                            }
+                        }
                     }
                 }
             }
         }
     }
+
+    private fun navigateToLoginAppActivity() {
+        val intent = Intent(this, LoginAppActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 
     /*
     private fun uploadJsonToFirebase() {
